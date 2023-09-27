118088c55b114cdbaf251d3618f7a105fc271551
abdul rohman <abdulrohmanunida@gmail.com>
2023-09-27 09:44:03 +0700

    connecting with MenuActivity

--- app/src/main/java/com/best/myappbar/MenuActivity.kt
+++ app/src/main/java/com/best/myappbar/MenuActivity.kt
@@ -2,10 +2,19 @@ package com.best.myappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.best.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+        }
     }
 }
