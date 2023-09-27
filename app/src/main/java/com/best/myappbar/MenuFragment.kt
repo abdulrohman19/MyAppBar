5c90981fa5104a3aebc90d3789f1b185c8b16c32
abdul rohman <abdulrohmanunida@gmail.com>
2023-09-27 17:39:24 +0700

    stuck activity menu

--- app/src/main/java/com/best/myappbar/MenuFragment.kt
+++ app/src/main/java/com/best/myappbar/MenuFragment.kt
@@ -7,10 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class MenuFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_menu_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    companion object {
     }
 }
